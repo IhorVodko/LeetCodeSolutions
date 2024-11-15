a2f5eccb640fb13da9eb7018b88d7b017089f1a8
Ihor Vodko <ihorfinance@gmail.com>
2024-11-15 13:34:46 +0200

    Create 1. Two Sum.cpp

---
+++ SolutionsAllProblems/1. Two Sum.cpp
@@ -0,0 +1,32 @@
+// problem :  https://leetcode.com/problems/two-sum/description/
+// submission : https://leetcode.com/problems/two-sum/submissions/1453436742
+
+// #include <vector>
+// #include <unordered_map>
+
+class Solution{
+public:
+    // let n = nums.size(), an input size
+    // time complexity O(n)
+    // space complexity O(n)
+    // Runtime 0 ms, beats 100 %
+    // Memory 14.38 MB, beats 36.48 %
+    std::vector<int> twoSum(
+        std::vector<int> & nums,
+        int target
+    ){
+        if(nums.size() == 2){
+            return {0, 1};
+        }
+        auto numToIndex{std::unordered_map<int, int>{}};
+        int diff{0};
+        auto secondNumIndex{std::begin(numToIndex)};
+        for(int index{0}; index < nums.size(); ++index){
+            diff = target - nums.at(index);
+            if(numToIndex.contains(diff)){
+                return {index, numToIndex.at(diff)};
+            }
+            numToIndex[nums.at(index)] = index;
+        }
+        return {-1, -1};
+    }
