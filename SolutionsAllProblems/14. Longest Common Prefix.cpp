3c54d0877005b2c4fcf78b3d12705c70f504a98b
Ihor Vodko <ihorfinance@gmail.com>
2024-11-19 20:31:55 +0200

    Create 14. Longest Common Prefix.cpp

---
+++ SolutionsAllProblems/14. Longest Common Prefix.cpp
@@ -0,0 +1,33 @@
+// problem :  https://leetcode.com/problems/longest-common-prefix/description/
+// submission : https://leetcode.com/problems/longest-common-prefix/submissions/1457529731
+// solution post: https://leetcode.com/problems/longest-common-prefix/solutions/6063354/c-modern-readable-code-beats-100-of-runtime
+
+// #include <algorithm>
+// #include <string>
+// #include <vector>
+
+class Solution {
+public:
+    // let 'S' be a sum of all letters from words in the input array
+    // time complexity O(S)
+    // space complexity O(1)
+    // Runtime 0 ms, beats 100 %
+    // Memory 11.15 mb, beats 74.91 %
+    std::string longestCommonPrefix(std::vector<std::string> & strs){
+        if(strs.empty()){
+            return "";
+        }
+        auto strMin{std::min_element(std::cbegin(strs), std::cend(strs), [&](
+            auto const & str1, auto const & str2){
+                return str1.length() < str2.length();})};
+        size_t indx{0};
+        for(; indx < strMin->size(); ++indx){
+            if(std::any_of(std::cbegin(strs), std::cend(strs),[=]
+                (auto const & str) {return str.at(indx) != strMin->at(indx);})
+            ){
+                break;
+            }
+        }
+        return strMin->substr(0, indx); 
+    }
+};
