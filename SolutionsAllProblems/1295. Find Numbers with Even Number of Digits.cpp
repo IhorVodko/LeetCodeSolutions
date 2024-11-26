b510b797adc7d64520f4777a31fc398199ac66f0
Ihor Vodko <ihorfinance@gmail.com>
2024-11-26 13:39:59 +0200

    Create 1295. Find Numbers with Even Number of Digits.cpp

---
+++ SolutionsAllProblems/1295. Find Numbers with Even Number of Digits.cpp
@@ -0,0 +1,22 @@
+// problem : https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
+// submission : https://leetcode.com/problems/find-numbers-with-even-number-of-digits/submissions/1463277372
+// solution post: https://leetcode.com/problems/find-numbers-with-even-number-of-digits/solutions/6085226/c-modern-readable-code-beats-100-runtime
+
+// #include <vector>
+// #include <algorithm>
+
+
+class Solution{
+public:
+    // let 'n' be an input length
+    // time complexity O(n)
+    // space complexity O(1)
+    // runtime 0 ms, beats 100 %
+    // memory 13.21 mb, beats 19.58 %
+    int findNumbers(std::vector<int> const & nums) const {
+        return std::count_if(nums.cbegin(), nums.cend(), [&] (auto num) {
+            return (10 <= num && num < 99) || (1000 <= num && num <= 9999) ||
+                num == 100000;
+        });
+    }
+};
