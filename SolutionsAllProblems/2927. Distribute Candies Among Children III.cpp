881c0f57a3b784c9c60edf7a3f6eede7e08ac956
Ihor Vodko <ihorfinance@gmail.com>
2025-06-05 17:48:05 +0300

    Create 2927. Distribute Candies Among Children III.cpp

---
+++ SolutionsAllProblems/2927. Distribute Candies Among Children III.cpp
@@ -0,0 +1,41 @@
+// problem : https://leetcode.com/problems/distribute-candies-among-children-iii/description/
+// submission : https://leetcode.com/problems/distribute-candies-among-children-iii/submissions/1654812867
+// solution post : https://leetcode.com/problems/distribute-candies-among-children-iii/solutions/6814064/
+//    c-modern-readable-code-beats-100-runtime-ut73
+
+class Solution {
+public:
+// time complexity O(1)
+// space complexity O(1)
+// runtime 0 ms, beats 100.00 %
+    long long distributeCandies(
+        int const candiesCount_,
+        int const candiesLimitPerChild_
+    ) const;
+private:
+    long long Calculate(long long const num_) const;
+};
+
+long long Solution::distributeCandies(
+    int const candiesCount_,
+    int const candiesLimitPerChild_
+) const {
+    auto const distributionsCountTotal{Calculate(candiesCount_ + 2)};
+    auto const atLeastOneChildMoreThanLimit{
+        3 * Calculate(candiesCount_ - candiesLimitPerChild_ + 1)};
+    auto const atLeastTwoChildrenMoreThanLimit{
+        3 * Calculate(candiesCount_ - (candiesLimitPerChild_ + 1) * 2 + 2)};
+    auto const threeChildrenMoreThanLimit{
+        Calculate(candiesCount_ - 3 * (candiesLimitPerChild_ + 1) + 2)};
+    auto const validDistributionsCount{
+            distributionsCountTotal
+        -   atLeastOneChildMoreThanLimit
+        +   atLeastTwoChildrenMoreThanLimit
+        -   threeChildrenMoreThanLimit
+    };
+    return validDistributionsCount; 
+}
+
+long long Solution::Calculate(long long const num_) const {
+    return num_ < 0 ? 0 : ((num_ - 1) * num_ / 2); 
+}
